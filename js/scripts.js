1cd360721f30203a751235260262f7c2c33418cb
ExDiego <diegosalas.j@gmail.com>
2024-06-09 06:29:26 -0400

    porfolio04

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function copymail(element) {
+    const tempInput = document.createElement('input');
+            // Asignar el texto del elemento al valor del input temporal
+            tempInput.value = element.innerText;
+            // Agregar el input temporal al body
+            document.body.appendChild(tempInput);
+            // Seleccionar el contenido del input temporal
+            tempInput.select();
+            // Ejecutar el comando de copiar
+            document.execCommand('copy');
+            // Eliminar el input temporal del DOM
+            document.body.removeChild(tempInput);
+            // Mostrar una alerta para indicar que el texto ha sido copiado
+            /* alert('Texto copiado: ' + tempInput.value); */
+}
