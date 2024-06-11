91ec84a0eee449ff6293c6a2566a960c9554270b
ExDiego <diegosalas.j@gmail.com>
2024-06-11 03:46:31 -0400

    tooltip logrado

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
 function copymail(element) {
-    const tempInput = document.createElement('input');
-            // Asignar el texto del elemento al valor del input temporal
-            tempInput.value = element.innerText;
-            // Agregar el input temporal al body
-            document.body.appendChild(tempInput);
-            // Seleccionar el contenido del input temporal
-            tempInput.select();
-            // Ejecutar el comando de copiar
-            document.execCommand('copy');
-            // Eliminar el input temporal del DOM
-            document.body.removeChild(tempInput);
-            // Mostrar una alerta para indicar que el texto ha sido copiado
-            /* alert('Texto copiado: ' + tempInput.value); */
+    // Copiar texto al portapapeles
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(element.innerText).then(function() {
+            showMessage();
+        }, function(err) {
+            console.error('No se pudo copiar el texto: ', err);
+        });
+    } else {
+        const tempInput = document.createElement('input');
+        tempInput.value = element.innerText;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempInput);
+        showMessage();
+    }
+}
+
+function showMessage() {
+    const copyBox = document.getElementById('copytext');
+    copyBox.classList.add('show');
+
+    // Quitar la clase después de 2 segundos (2000 milisegundos)
+    setTimeout(() => {
+        copyBox.classList.remove('show');
+    }, 800);
 }
